caef43e0375b5965f1616a1dc10a723de086ebfe
RAFAEL FERREIRA LOPES <rafael.ferreira-ext@viavarejo.com.br>
2021-04-29 22:34:37 -0300

    Frontend pcdevs

--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -1,6 +1,6 @@
+import { ClienteModel } from './../model/cliente-model';
 import { ClienteDTO } from './../dto/cliente-dto';
 import { Observable } from 'rxjs';
-import { ClienteModel } from './../model/cliente-model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
@@ -11,22 +11,22 @@ import { environment } from '../../environments/environment';
   })
 export class ClienteService {
 
-    url: string = environment.URLSERVIDOR;
+    url: string = environment.URLSERVIDOR+'cliente';
 
     constructor(private http: HttpClient) { }
   
     buscar(): Observable<ClienteModel[]>{
-      return this.http.get(this.url+'cliente')
-            .pipe( map(resposta => <ClienteModel[]> resposta) );
+      return this.http.get(this.url)
+          .pipe( map( resposta => <ClienteModel[]> resposta) );
     }
 
     salvar(cliente: ClienteDTO): Observable<ClienteModel>{
-      return this.http.post(this.url+'cliente', cliente)
+      return this.http.post(this.url, cliente)
             .pipe( map(resposta => <ClienteModel> resposta) );
     }
 
     atualizar(cliente: ClienteDTO){
-      return this.http.put(this.url+'cliente/'+cliente.id, cliente)
+      return this.http.put(this.url+'/'+cliente.id, cliente)
       .pipe( map(resposta => <ClienteModel> resposta) );
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  URLSERVIDOR: 'http://localhost:8080/'
+  URLSERVIDOR: 'http://localhost:8081/'
 };
 
 /*
